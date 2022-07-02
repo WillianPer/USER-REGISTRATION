8d118183e0b5055989e3e07640e15616c3e9b81b
WilianPer <wsresilienttasteshopw@gmail.com>
2022-07-02 16:42:53 -0400

    Aplicando o componente Cadastro de usuário aula 3!!

--- crud/frontend/src/components/user/UserCrud.jsx
+++ crud/frontend/src/components/user/UserCrud.jsx
@@ -28,21 +28,69 @@ export default class UserCrud extends Component {
         const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
         axios[method](url, user)
             .then(resp => {
-                const list = this.getUpdateList(resp.data)
+                const list = this.getUpdatedList(resp.data)
                 this.setState({user: initialState.user, list})
             })
     }
 
-    getUpdateList(user) {
+    getUpdatedList(user) {
         const list = this.state.list.filter(u => u.id !== user.id)
         list.unshift(user)
         return list
     }
 
+    updateField(event){
+        const user = { ...this.state.user }
+        user[event.target.name] = event.target.name
+        this.setState({user})
+    }
+
+    renderForm() {
+        return (
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <label>Nome</label>
+                        <input type="text" name="name" className="form-control"
+                        value={this.state.user.name} 
+                        onChange={e => this.updateField(e)} 
+                        placeholder="Digite o nome..." />
+                    </div>
+
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Email</label>
+                            <input type="text" name="email" className="form-control"
+                            value={this.state.user.email} 
+                            onChange={e => this.updateField(e)} 
+                            placeholder="Digite o Email..." />
+                        </div>
+                    </div>
+                </div>
+
+                <hr />
+
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-end">
+                        <botton className="btn btn-prymary"
+                        onClick = {e => this.save(e)}>
+                            Salvar
+                        </botton>
+
+                        <botton className="btn btn-secondary ml-2"
+                        onClick = {e => this.clear(e)}>
+                            cancelar
+                        </botton>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     render() {
         return (
             <Main { ...headerProps}>
-                Cadastro de Usuário!
+                {this.renderForm()}
             </Main>
         )
     }
